68a1a590101ab7587df2c510e765108be53fa55f
saroyar <yarsaro2001@gmail.com>
2023-02-09 16:19:08 +0530

    mongodb update

--- database/moncon.js
+++ database/moncon.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose");
-// const uri="mongodb+srv://saroyar:saroyar43@cluster0.6oa8trz.mongodb.net/todo_list?retryWrites=true&w=majority";
-const uri="mongodb://localhost:27017/todolist";
+const uri="mongodb+srv://saroyar:saroyar43@cluster0.6oa8trz.mongodb.net/todo_list?retryWrites=true&w=majority";
+// const uri="mongodb://localhost:27017/todolist";
 
 exports.databaseconnect=()=>{
     mongoose.connect(uri,()=>{
