a45991f981b10cddd8e2c0d62218cb73ab0bc4e8
saroyar <yarsaro2001@gmail.com>
2023-02-10 15:40:58 +0530

    Access-Control

--- server.js
+++ server.js
@@ -29,6 +29,12 @@ app.use(cors({
     origin:["http://localhost:3000"]
 }));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 const router = express.Router();
 
 router.get("/", (req, res) => {
