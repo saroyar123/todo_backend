96669adac4485ce0e3bc60f59fc70c7827d48588
saroyar <yarsaro2001@gmail.com>
2023-02-10 15:18:01 +0530

    cors update

--- server.js
+++ server.js
@@ -1,41 +1,52 @@
-const express=require("express");
+const express = require("express");
 const { Authitication } = require("./config/auth");
-const { login, register, logout, deleteAccount, addTasks, deleteTask, getUserData, authenticate } = require("./controller/userFuntions");
-const {databaseconnect}=require("./database/moncon");
-const User=require("./model/user");
-const cookieparser=require("cookie-parser");
-// const dotenv=require("dotenv");
-const dotenv=require('dotenv');
-const cors=require('cors');
+const {
+  login,
+  register,
+  logout,
+  deleteAccount,
+  addTasks,
+  deleteTask,
+  getUserData,
+  authenticate,
+} = require("./controller/userFuntions");
+const { databaseconnect } = require("./database/moncon");
+const User = require("./model/user");
+const cookieparser = require("cookie-parser");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
 dotenv.config();
 
 
-
-const app=express();
+const app = express();
 databaseconnect();
 app.use(express.json());
 app.use(cookieparser());
-app.use(cors());
+app.use(cors({
+    credentials:true,
+    methods:["GET","POST","PUT","DELETE"],
+    origin:["*"]
+}));
 
-const router=express.Router();
+const router = express.Router();
 
-router.get('/',(req,res)=>{
-    res.status(200).json({
-        success:true,
-        message:"server is running"
-    })
-})
-router.post("/login",login);
-router.post("/register",register);
-router.get("/logout",Authitication,logout);
-router.delete("/deleteAccount",Authitication,deleteAccount);
-router.post("/addTask",Authitication,addTasks);
-router.delete("/deleteTask/:id",Authitication,deleteTask);
-router.get("/getUserData",Authitication,getUserData);
+router.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "server is running",
+  });
+});
+router.post("/login", login);
+router.post("/register", register);
+router.get("/logout", Authitication, logout);
+router.delete("/deleteAccount", Authitication, deleteAccount);
+router.post("/addTask", Authitication, addTasks);
+router.delete("/deleteTask/:id", Authitication, deleteTask);
+router.get("/getUserData", Authitication, getUserData);
 
-app.use("/api",router);
+app.use("/api", router);
 
-app.listen(process.env.PORT||1234,()=>{
-    console.log(`server runing on port ${process.env.PORT}`)
-})
+app.listen(process.env.PORT || 1234, () => {
+  console.log(`server runing on port ${process.env.PORT}`);
+});
